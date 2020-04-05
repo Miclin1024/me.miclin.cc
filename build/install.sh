e2581bab9921dc6b5bad541ae9fbef40f44d80aa
Michael Lin <miclin@berkeley.edu>
2020-04-04 18:07:33 -0700

    Create deploy script

--- build/install.sh
+++ build/install.sh
@@ -2,6 +2,6 @@
 ssh -i ~/.ssh/id_rsa -t root@me.miclin.cc "
     cd /tmp && git clone https://github.com/Miclin1024/me.miclin.cc site;
     cp -rfv /tmp/site/* /var/www/me/
-    
+    chown -R www-data:www-data /var/www/me*
     rm -rf /tmp/site
 "
